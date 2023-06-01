316c7ec2076e514e5674c00a0562b217321bf39a
feh-franc0 <fernandofv1110@gmail.com>
2023-06-01 16:50:57 -0300

    Functional Error Handling

---
+++ src/core/either.ts
@@ -0,0 +1,33 @@
+// ? Error
+export class Left<L> {
+  readonly value: L
+
+  constructor(value: L) {
+    this.value = value
+  }
+}
+
+// ? Success
+export class Right<R> {
+  readonly value: R
+
+  constructor(value: R) {
+    this.value = value
+  }
+}
+
+export type Either<L, R> = Left<L> | Right<R>
+
+export const left = <L, R>(value: L): Either<L, R> => {
+  return new Left(value)
+}
+
+export const right = <L, R>(value: R): Either<L, R> => {
+  return new Right(value)
+}
+
+// * Left / Right
+
+// * export class /
+
+// * UI -> CONTROLLER -> CASO DE USO -> ENTIDADE -> CASO DE USO -> REPOSITÓRI -> BANCO DE DADOS
